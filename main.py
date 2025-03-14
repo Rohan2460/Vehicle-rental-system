7ce2b0679f4dc546ed0d455f86221a0c5667ffbf
Rohan <154054727+Rohan2460@users.noreply.github.com>
2025-03-14 22:28:34 +0530

    testing tables

--- main.py
+++ main.py
@@ -9,18 +9,32 @@ app = Flask(__name__)
 def index():
     cursor.execute("SELECT * FROM Bookings")
     data = cursor.fetchall()
-    print(data)
+    # print(data)
+    cursor.execute("SELECT Name FROM Customer WHERE CustomerID = 1")
+    customer = cursor.fetchone()
+
     
-    return render_template("submit.html", data=data)
+    return render_template("submit.html", data=data, customer=customer[0])
 
 @app.route("/data", methods=["POST"])
 def get_data():
     if request.method == "POST":
         data = request.form["data"]
-        cursor.execute(f"INSERT INTO Bookings (status) VALUES ('{data}')")
+        amount = request.form["amount"]
+        c_id = request.form["c_id"]
+
+        cursor.execute(f"INSERT INTO Bookings (BookingStatus, TotalAmount, CustomerID) VALUES ('{data}', {amount}, {c_id})")
         cnx.commit()
 
     return redirect("/")
+
+
+@app.route("/user", methods=["GET"])
+def create_user():
+    cursor.execute(f"INSERT INTO Customer (Name, Email) VALUES ('John', 'john@a.com')")
+    cnx.commit()
+    return redirect("/")
+
 
 if __name__ == "__main__":
     app.run()
--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
-from db import cursor
+from db import cursor, cnx
 
 cursor.execute("CREATE DATABASE vehicle_rental_db")
 cursor.execute("USE vehicle_rental_db")
-cursor.execute("CREATE TABLE Bookings (BookingID INT NOT NULL AUTO_INCREMENT PRIMARY KEY, status VARCHAR(255))")
+cursor.execute("CREATE TABLE Customer (CustomerID INT NOT NULL AUTO_INCREMENT PRIMARY KEY, Name VARCHAR(255), Email VARCHAR(255))")
+cursor.execute("CREATE TABLE Bookings (BookingID INT NOT NULL AUTO_INCREMENT PRIMARY KEY, BookingStatus VARCHAR(255), TotalAmount INT, CustomerID INT, FOREIGN KEY (CustomerID) REFERENCES Customer(CustomerID))")
+cnx.commit()
 # schema here 
